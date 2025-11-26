ec9576e6940f33bb7240c19ce604858e0dc267c5
Anamika <anamika2004abc@gmail.com>
2025-11-26 13:17:24 +0530

    Done!!!

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/navbar.jsx'
+import Home from './components/Home.jsx'
+import Footer from './components/Footer.jsx'
+
+function App(){
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
+
+
